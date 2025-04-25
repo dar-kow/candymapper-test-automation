516bc5ec847730cf13c2cb952c2d7722c97cc025
Darek <darecki9k@gmail.com>
2025-04-25 17:44:00 +0200

    feat: implement navigation actions, components, data, and tests for CandyMapper

---
+++ tests/navigation/components.ts
@@ -0,0 +1,44 @@
+export class NavigationComponents {
+  // Popup selectors
+  popupCloseButton = '#popup-widget307423-close-icon';
+  popup = '#popup-widget307423';
+
+  // Main navigation selectors
+  mainNav = 'ul[data-ux="NavFooter"]';
+  visibleNavItems = 'ul[id="nav-307305"] li[style*="visibility: visible"]';
+  navLinks = 'a[data-ux="NavLink"]';
+
+  // More dropdown selectors
+  moreButton = 'a[data-ux="NavLinkDropdown"][data-aid="NAV_MORE"]';
+  moreDropdown = 'ul[id="more-307306"]';
+  moreDropdownItems = 'ul[id="more-307306"] li[style*="visibility: visible"]';
+  moreDropdownLinks = 'a[data-ux="NavMoreMenuLink"]';
+
+  // Specific page elements selectors
+  loginEmailField = 'input[data-aid="MEMBERSHIP_SSO_LOGIN_EMAIL"]';
+  loginPasswordField = 'input[data-aid="MEMBERSHIP_SSO_LOGIN_PASSWORD"]';
+  loginSubmitButton = 'input[data-aid="MEMBERSHIP_SSO_SUBMIT"]';
+
+  // id variable that's why I use div attribute + nesting
+  iframe = 'div[data-ux="Element"] iframe';
+  loader = 'div.loader';
+
+  authorBioSection = '#author-bio';
+
+  findMyCandyTitle = '[data-aid="HTML_SECTION_TITLE_RENDERED"]';
+
+  automationSandboxTitle = '[data-aid="CONTENT_SECTION_TITLE_RENDERED"]';
+
+  momTitle = '[data-aid="CONTENT_SECTION_TITLE_RENDERED"]';
+
+  sandboxToolsButtons = [
+    '[data-aid="CONTENT_CTA_BTN1_RENDERED"]',
+    '[data-aid="CONTENT_CTA_BTN2_RENDERED"]',
+    '[data-aid="CONTENT_CTA_BTN3_RENDERED"]',
+    '[data-aid="CONTENT_CTA_BTN4_RENDERED"]',
+  ];
+
+  vampirasBlogTitle = '[data-aid="RSS_SECTION_TITLE_RENDERED"]';
+
+  email = '#email';
+}
