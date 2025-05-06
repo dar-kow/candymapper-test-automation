54e3a9c4aa1fa83f2b44fe48a0b0dc93a592b0bd
Darek <darecki9k@gmail.com>
2025-05-06 08:22:44 +0200

    refactor: standardize quotation marks in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: '.',
-  testMatch: '**/test.ts',
+  testDir: ".",
+  testMatch: "**/test.ts",
   fullyParallel: true,
   timeout: 30000,
   expect: {
@@ -11,30 +11,30 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 3,
-  reporter: [['html', { open: process.env.CI ? 'never' : 'on-failure' }], ['list']],
+  reporter: [["html", { open: process.env.CI ? "never" : "on-failure" }], ["list"]],
   use: {
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
 
     viewport: { width: 1366, height: 768 },
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: 'chrome',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chrome",
+      use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: 'safari',
-      use: { ...devices['Desktop Safari'] },
+      name: "safari",
+      use: { ...devices["Desktop Safari"] },
     },
   ],
 });
